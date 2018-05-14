7541622c0b7d8e97ecb41b7fb38e6a4ed5109137
kalyan5694 <kalyankumarchinnagiri594@gmail.com>
2018-05-14 10:33:59 +0530

    Renew Commit

--- HelloWorld/src/com/app/model/HelloWorld.java
+++ HelloWorld/src/com/app/model/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 
 	 public static void main(String[] args) {
 		System.out.println("HelloWorld My first Program");
-		System.out.println("I got it.");
+		//System.out.println("I got it.");
 	}
 }
